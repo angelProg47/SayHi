e4085689cb492f779074fae2af1c6d6ea35e64f1
angelProg47 <angelmimikyufav@gmail.com>
2024-04-25 16:33:15 -0700

    todo esto fue instalado en SayHi

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>y el snicker que te di wey >:c</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2CFBFE',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
